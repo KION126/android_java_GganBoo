e38f3bddb4b1a22a422abe58672ee1ad83a6826a
KION <kg20011216@gmail.com>
2024-05-31 14:38:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/gganboo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.gganboo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnSignup;
+    private Button btnSignin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnSignup = (Button)findViewById(R.id.btnSignUp);       // 회원가입
+        btnSignin = (Button)findViewById(R.id.btnSignIn);       // 로그인
+        btnSignup.setOnClickListener(this);
+        btnSignin.setOnClickListener(this);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnSignup){
+            Intent signIntent = new Intent(MainActivity.this, SignupActivity.class);
+            startActivity(signIntent);
+        }
+    }
+}
