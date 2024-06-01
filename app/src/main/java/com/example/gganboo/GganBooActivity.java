4ec60378ad0aea9829126a7ed56a5666b93654fa
hwangjihong <wlghd6737@gmail.com>
2024-06-02 01:27:44 +0900

    Bottom Navigation Bar 생성

---
+++ app/src/main/java/com/example/gganboo/GganBooActivity.java
@@ -0,0 +1,63 @@
+package com.example.gganboo;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.gganboo.databinding.ActivityGganbooBinding;
+import com.example.gganboo.navbar.CalendarFragment;
+import com.example.gganboo.navbar.MyFragment;
+import com.example.gganboo.navbar.NotificationFragment;
+import com.example.gganboo.navbar.SearchFragment;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class GganBooActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    private ActivityGganbooBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityGganbooBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new CalendarFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(this);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.calendarNav) {
+            replaceFragment(new CalendarFragment());
+        } else if (itemId == R.id.searchNav) {
+            replaceFragment(new SearchFragment());
+        } else if (itemId == R.id.notificationNav) {
+            replaceFragment(new NotificationFragment());
+        } else if(itemId == R.id.myNav){
+            replaceFragment(new MyFragment());
+        }
+        return true;
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
